89273a2ee70d5964d03247e7b16670fd50940947
DaveKearney <skyclad@gmail.com>
2025-03-31 15:37:18 +0000

    Deploying to gh-pages from @ DaveKearney/brandium@729b68530ba16f9f19c1c5cb1f2b4ff309361c03 🚀

---
+++ _next/static/chunks/app/terms/page-ff26254316a5fd42.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[571,301,200,931,979,385],{274:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=274)}),_N_E=n.O()}]);
